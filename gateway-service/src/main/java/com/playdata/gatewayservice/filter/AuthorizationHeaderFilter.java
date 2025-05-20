54f65cf461721d59fa228f6ef030117d60026a6e
ubing <ubing0101@kakao.com>
2025-05-20 17:47:31 +0900

    변경이 발생한 서비스만 골라서 빌드하는 스테이지와 로직을 추가 - 4

--- gateway-service/src/main/java/com/playdata/gatewayservice/filter/AuthorizationHeaderFilter.java
+++ gateway-service/src/main/java/com/playdata/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -30,6 +30,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory {
     private final List<String> allowUrl = Arrays.asList(
             "/user/create", "/user/doLogin", "/user/refresh",
             "/product/list", "/user/health-check"
+
     );
 
     @Override
