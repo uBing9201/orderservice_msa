ab872783e3771bc468890513e3c19a2c22588589
ubing <ubing0101@kakao.com>
2025-05-29 11:42:37 +0900

    카카오 로그인 포트번호 수정

--- user-service/src/main/java/com/playdata/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/playdata/userservice/user/controller/UserController.java
@@ -11,22 +11,25 @@ import com.playdata.userservice.user.entity.User;
 import com.playdata.userservice.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user") // user 관련 요청은 /user로 시작한다고 가정.
@@ -246,7 +249,7 @@ public class UserController {
                             }, 'http://localhost:5174');
                             window.close();
                         } else {
-                            window.location.href = 'http://localhost:5173';
+                            window.location.href = 'http://localhost:5174';
                         }
                     </script>
                     <p>카카오 로그인 처리 중...</p>
